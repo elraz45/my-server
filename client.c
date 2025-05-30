0c830f6abe2bf459b36cca142cc215b6803bb72b
Elraz Hamtsani <elraz@example.com>
2025-05-30 12:13:22 +0000

    Start clean repo

---
+++ client.c
@@ -0,0 +1,126 @@
+/*
+ * client.c: A very, very primitive HTTP client.
+ *
+ * Example usage:
+ *      ./client www.example.com 80 / GET 5
+ *
+ * This client sends multiple HTTP requests to a server using threads.
+ */
+
+#include "segel.h"
+#include <pthread.h>
+
+// Sends an HTTP request to the server using the given socket
+void clientSend(int fd, char *filename, char *method)
+{
+    char buf[MAXLINE];
+    char hostname[MAXLINE];
+
+    Gethostname(hostname, MAXLINE); // Get the client's hostname
+
+    // Form the HTTP request line and headers
+    sprintf(buf, "%s %s HTTP/1.1\n", method, filename);
+    sprintf(buf, "%shost: %s\n\r\n", buf, hostname);
+
+    printf("Request:\n%s\n", buf); // Display the request for debugging
+
+    // Send the request to the server
+    Rio_writen(fd, buf, strlen(buf));
+}
+
+// Reads and prints the server's HTTP response
+void clientPrint(int fd)
+{
+    rio_t rio;
+    char buf[MAXBUF];
+    int length = 0;
+    int n;
+
+    // Initialize buffered input
+    Rio_readinitb(&rio, fd);
+
+    // --- Read and print HTTP headers ---
+    n = Rio_readlineb(&rio, buf, MAXBUF);
+    while (strcmp(buf, "\r\n") && (n > 0))
+    {
+        printf("Header: %s", buf);
+
+        // Try to parse Content-Length header
+        if (sscanf(buf, "Content-Length: %d ", &length) == 1)
+        {
+            printf("Length = %d\n", length);
+        }
+
+        n = Rio_readlineb(&rio, buf, MAXBUF);
+    }
+
+    // --- Read and print HTTP body ---
+    n = Rio_readlineb(&rio, buf, MAXBUF);
+    while (n > 0)
+    {
+        printf("%s", buf);
+        n = Rio_readlineb(&rio, buf, MAXBUF);
+    }
+}
+
+// Struct to pass args to each thread
+typedef struct
+{
+    char *host;
+    int port;
+    char *filename;
+    char *method;
+    int id;
+} client_args;
+
+void *client_thread(void *arg)
+{
+    client_args *args = (client_args *)arg;
+    int clientfd = Open_clientfd(args->host, args->port);
+    if (clientfd < 0)
+    {
+        fprintf(stderr, "[Thread %d] Error connecting to %s:%d\n", args->id, args->host, args->port);
+        return NULL;
+    }
+
+    printf("[Thread %d] Connected to server.\n", args->id);
+    clientSend(clientfd, args->filename, args->method);
+    clientPrint(clientfd);
+    Close(clientfd);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 6)
+    {
+        fprintf(stderr, "Usage: %s <host> <port> <filename> <method> <num_threads>\n", argv[0]);
+        exit(1);
+    }
+
+    char *host = argv[1];
+    int port = atoi(argv[2]);
+    char *filename = argv[3];
+    char *method = argv[4];
+    int num_threads = atoi(argv[5]);
+
+    pthread_t threads[num_threads];
+    client_args args[num_threads];
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        args[i].host = host;
+        args[i].port = port;
+        args[i].filename = filename;
+        args[i].method = method;
+        args[i].id = i + 1;
+        pthread_create(&threads[i], NULL, client_thread, &args[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,130 @@
+#include "segel.h"
+#include "request.h"
+#include "log.h"
+#include "queue.h"
+#include <pthread.h>
+
+pthread_mutex_t m;
+pthread_cond_t not_empty;
+pthread_cond_t not_full;
+
+Queue wait_queue = NULL;
+server_log sharedLog;
+
+void getargs(int *port, int *thread_num, int *queue_size, int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        fprintf(stderr, "Usage: %s <port> <threads> <queue_size>\n", argv[0]);
+        exit(1);
+    }
+    *port = atoi(argv[1]);
+    *thread_num = atoi(argv[2]);
+    *queue_size = atoi(argv[3]);
+}
+
+// Thread function for the worker pool
+void *worker_thread(void *arg)
+{
+    int thread_id = *(int *)arg;
+    threads_stats t = malloc(sizeof(struct Threads_stats));
+    t->id = thread_id;
+    t->stat_req = 0;
+    t->dynm_req = 0;
+    t->total_req = 0;
+
+    while (1)
+    {
+        pthread_mutex_lock(&m);
+
+        while (queue_empty(wait_queue))
+        {
+            pthread_cond_wait(&not_empty, &m);
+        }
+
+        struct timeval arrival = queue_head_arrival_time(wait_queue);
+        int connfd = dequeue(wait_queue);
+
+        pthread_cond_signal(&not_full);
+        pthread_mutex_unlock(&m);
+
+        struct timeval dispatch;
+        gettimeofday(&dispatch, NULL);
+
+        requestHandle(connfd, arrival, dispatch, t, sharedLog);
+        Close(connfd);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    int port, threads_num, queue_max_size;
+    getargs(&port, &threads_num, &queue_max_size, argc, argv);
+
+    // Create the shared log
+    sharedLog = create_log();
+
+    // Init queue and synchronization
+    wait_queue = queue_create(queue_max_size);
+    pthread_mutex_init(&m, NULL);
+    pthread_cond_init(&not_empty, NULL);
+    pthread_cond_init(&not_full, NULL);
+
+    // Thread pool
+    pthread_t *threads = malloc(sizeof(pthread_t) * threads_num);
+    for (int i = 0; i < threads_num; i++)
+    {
+        int *tid = malloc(sizeof(int));
+        *tid = i;
+        pthread_create(&threads[i], NULL, worker_thread, tid);
+    }
+
+    // Start listening
+    int listenfd = Open_listenfd(port);
+    while (1)
+    {
+        struct sockaddr_in clientaddr;
+        int clientlen = sizeof(clientaddr);
+        int connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *)&clientlen);
+
+        struct timeval arrival;
+        gettimeofday(&arrival, NULL);
+
+        pthread_mutex_lock(&m);
+
+        /* not drop tail -
+
+        while (queue_full(wait_queue))
+        {
+            pthread_cond_wait(&not_full, &m);
+        }
+        enqueue(wait_queue, connfd, arrival);
+
+        pthread_cond_signal(&not_empty);
+        pthread_mutex_unlock(&m);
+
+        */
+
+        if (queue_full(wait_queue))
+        {
+            Close(connfd); // Drop tail: just close the connection
+        }
+        else
+        {
+            enqueue(wait_queue, connfd, arrival);
+            pthread_cond_signal(&not_empty);
+        }
+        pthread_mutex_unlock(&m);
+    }
+
+    // Cleanup
+    destroy_log(sharedLog);
+    queue_destroy(wait_queue);
+    pthread_mutex_destroy(&m);
+    pthread_cond_destroy(&not_empty);
+    pthread_cond_destroy(&not_full);
+
+    return 0;
+}
